4720393767fdd691daaa5c70164f7be569ac105e
lililala92 <1205954738@qq.com>
2017-06-16 15:04:32 +0800

    angular

---
+++ The Examples/angularjs/script.js
@@ -0,0 +1,28 @@
+var myApp=angular.module('myapp',['ngRoute']);
+
+myApp.config(function($routeProvider){
+   $routeProvider
+
+       .when('/',{
+       	  templateUrl:'pages/home.html',
+       	  controller:'mainController'
+       })
+       .when('/about',{
+       	  templateUrl:'pages/about.html',
+       	  controller:'aboutController'
+       })
+       .when('/contact',{
+       	  templateUrl:'pages/contact.html',
+       	  controller:'contactController'
+       })
+
+});
+myApp.controller('mainController',function($scope){
+	$scope.message='Everyone come and see how good I look!';
+});
+myApp.controller('aboutController',function($scope){
+	$scope.message='Look! I am an about page.';
+});
+myApp.controller('contactController',function($scope){
+	$scope.message='Contact us! JK. This is just a demo.';
+});
